42b084146631b22ce6d0f50ada0b3e25efa66e95
Leonardo <leonardo2sc@gmail.com>
2021-08-12 18:56:40 -0300

    Django Admin Update

--- ArticlesApi/api/admin.py
+++ ArticlesApi/api/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from api.models import Article, Author
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ["=id", "name"]
 
-admin.site.register(Article)
-admin.site.register(Author)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "author"]
+    search_fields = ["=id", "title", "category"]
+    list_filter = ["author", "category"]
+    autocomplete_fields = ["author"]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Author, AuthorAdmin)
